2f7e90e0ab250a6f67e9f29426b8ce4fab3f3db9
wuyihua <wuyihua86@gmail.com>
2015-11-13 13:30:49 +0800

    提交最新的实体类

--- src/main/java/com/springapp/mvc/entities/GiftEntity.java
+++ src/main/java/com/springapp/mvc/entities/GiftEntity.java
@@ -1,92 +1,91 @@
-package com.springapp.mvc.entities;
-
-import javax.persistence.*;
-
-/**
- * Created by glpublic on 2015/11/11.
- */
-@Entity
-@Table(name = "gift", schema = "", catalog = "confidant")
-public class GiftEntity {
-    private String id;
-    private int userId;
-    private Integer count;
-    private Integer fromUserId;
-    private Integer itemId;
-
-    @Basic
-    @Column(name = "id")
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    @Id
-    @Column(name = "userId")
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
-
-    @Basic
-    @Column(name = "count")
-    public Integer getCount() {
-        return count;
-    }
-
-    public void setCount(Integer count) {
-        this.count = count;
-    }
-
-    @Basic
-    @Column(name = "fromUserId")
-    public Integer getFromUserId() {
-        return fromUserId;
-    }
-
-    public void setFromUserId(Integer fromUserId) {
-        this.fromUserId = fromUserId;
-    }
-
-    @Basic
-    @Column(name = "itemId")
-    public Integer getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Integer itemId) {
-        this.itemId = itemId;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        GiftEntity that = (GiftEntity) o;
-
-        if (userId != that.userId) return false;
-        if (id != null ? !id.equals(that.id) : that.id != null) return false;
-        if (count != null ? !count.equals(that.count) : that.count != null) return false;
-        if (fromUserId != null ? !fromUserId.equals(that.fromUserId) : that.fromUserId != null) return false;
-        if (itemId != null ? !itemId.equals(that.itemId) : that.itemId != null) return false;
-
-        return true;
-    }
-
-    @Override
-    public int hashCode() {
-        int result = id != null ? id.hashCode() : 0;
-        result = 31 * result + userId;
-        result = 31 * result + (count != null ? count.hashCode() : 0);
-        result = 31 * result + (fromUserId != null ? fromUserId.hashCode() : 0);
-        result = 31 * result + (itemId != null ? itemId.hashCode() : 0);
-        return result;
-    }
-}
+package com.springapp.mvc.entities;
+import java.io.Serializable;
+import javax.persistence.*;
+
+/**
+ * Created by glpublic on 2015/11/13.
+ */
+@Entity
+@Table(name = "gift", schema = "", catalog = "confidant")
+public class GiftEntity implements Serializable {    private String id;
+    private int userId;
+    private Integer count;
+    private Integer fromUserId;
+    private Integer itemId;
+
+    @Basic
+    @Column(name = "id")
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Id
+    @Column(name = "userId")
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    @Basic
+    @Column(name = "count")
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
+
+    @Basic
+    @Column(name = "fromUserId")
+    public Integer getFromUserId() {
+        return fromUserId;
+    }
+
+    public void setFromUserId(Integer fromUserId) {
+        this.fromUserId = fromUserId;
+    }
+
+    @Basic
+    @Column(name = "itemId")
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        GiftEntity that = (GiftEntity) o;
+
+        if (userId != that.userId) return false;
+        if (id != null ? !id.equals(that.id) : that.id != null) return false;
+        if (count != null ? !count.equals(that.count) : that.count != null) return false;
+        if (fromUserId != null ? !fromUserId.equals(that.fromUserId) : that.fromUserId != null) return false;
+        if (itemId != null ? !itemId.equals(that.itemId) : that.itemId != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id != null ? id.hashCode() : 0;
+        result = 31 * result + userId;
+        result = 31 * result + (count != null ? count.hashCode() : 0);
+        result = 31 * result + (fromUserId != null ? fromUserId.hashCode() : 0);
+        result = 31 * result + (itemId != null ? itemId.hashCode() : 0);
+        return result;
+    }
+}
--- src/main/webapp/WEB-INF/script/addSerelirableTool.py
+++ src/main/webapp/WEB-INF/script/addSerelirableTool.py
@@ -1,7 +1,33 @@
-#!/usr/bin/python
-import sys,os
-f=open('hi.txt','r+')
-flist=f.readlines()
-flist[4]='hi\n'
-f=open('hi.txt','w+')
-f.writelines(flist)
+# -*- coding: utf-8 -*-
+import os
+import shutil
+
+FOLDER_PATH = "E:/ConfidantServer/src/main/java/com/springapp/mvc/entities/";
+
+
+def writeSeriliable2File(filePath, fileName):
+    f = open(filePath, "rb+");
+    flist = f.readlines()
+
+    for (index, char) in enumerate(flist):
+        line_str = str(flist[index])
+        if (line_str.find("class") != -1 and line_str.find("Serializable")==-1):
+            flist[1] = "import java.io.Serializable;" + flist[1]
+            flist[index] = "public class " + fileName.split(".")[0] + " implements Serializable {"
+            print flist[index]
+    f.close()
+    f = open(filePath, "w")
+    f.writelines(flist)
+    f.close()
+
+
+def beginWrite(folderPath):
+    if not os.path.isdir(folderPath):
+        return
+    for filename in os.listdir(folderPath):
+        writeSeriliable2File(folderPath + filename, filename);
+        print filename
+
+
+if __name__ == '__main__':
+    beginWrite(FOLDER_PATH);
