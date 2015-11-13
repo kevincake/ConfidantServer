b7a436b14c0965404664e5a189315b35ef5d8241
wuyihua <wuyihua86@gmail.com>
2015-11-13 17:51:39 +0800

    提交BlogService

--- src/main/java/com/springapp/mvc/controller/BlogController.java
+++ src/main/java/com/springapp/mvc/controller/BlogController.java
@@ -1,7 +1,15 @@
 package com.springapp.mvc.controller;
 
+import com.springapp.mvc.service.BlogService;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by glpublic on 2015/11/11.
  */
+@RequestMapping("/blog")
 public class BlogController {
+    public void getBolgListByUserId(int userId){
+        BlogService service = new BlogService();
+        
+    }
 }
---
+++ src/main/java/com/springapp/mvc/service/BlogService.java
@@ -0,0 +1,45 @@
+package com.springapp.mvc.service;
+
+import com.springapp.mvc.dao.DBUtils;
+import com.springapp.mvc.entities.BlogEntity;
+import com.springapp.mvc.entities.UserEntity;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.hibernate.criterion.Restrictions;
+
+import java.util.List;
+
+/**
+ * Created by glpublic on 2015/11/13.
+ */
+public class BlogService {
+    //获取博客列表，分页
+    public List<BlogEntity> getBlogList(int userId,int pageNum, int pageSize) {
+        long time = System.currentTimeMillis();
+        Session session = DBUtils.getSession();
+        Criteria lastC = session.createCriteria(BlogEntity.class);
+        lastC.add(Restrictions.eq("userId", userId));
+        lastC.add(Restrictions.lt("time", time));
+        lastC.setFirstResult((pageNum - 1) * pageSize);
+        lastC.setMaxResults(pageSize);
+        List<BlogEntity> blogList = lastC.list();
+        return blogList;
+    }
+    //发布博客
+    public BlogEntity saveBlog(BlogEntity blog) {
+        Session session = DBUtils.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.save(blog);
+        transaction.commit();
+        return blog;
+    }
+    public boolean deleteBlog(BlogEntity blog){
+        Session session = DBUtils.getSession();
+        Transaction transaction = session.beginTransaction();
+        session.delete(blog);
+        transaction.commit();
+        return true;
+    }
+
+}
